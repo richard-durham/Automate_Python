c85ce78845b097c84b71f66997cf06325d218ed6
Richard Durham <rdurham@espial.local>
2016-04-12 11:22:58 +0100

    code from book that finds phone numbers - using as learning playground

---
+++ demoPhoneRegex.py
@@ -0,0 +1,14 @@
+
+import re
+
+phoneRegex = re.compile(r'''(
+    (\d{3}|\(\d{3}\))?            # area code
+    (\s|-|\.)?                    # separator
+    \d{3}                         # first 3 digits
+    (\s|-|\.)                     # separator
+    \d{4}                         # last 4 digits
+    (\s*(ext|x|ext.)\s*\d{2,5})?  # extension
+    )''', re.VERBOSE)
+
+phoneNumber = phoneRegex.search('hello world (555)222-4589 ext 911 mary had a little lamb')
+print(phoneNumber.group(1))
