533a9aef69f62345299143ccb2204c726a264815
Richard Durham <rdurham@espial.local>
2016-04-12 11:24:59 +0100

    beginning of Practice Program to strip leading/trailing spaces or charaters provided in argument from string

---
+++ stripper.py
@@ -0,0 +1,31 @@
+
+'''
+Automate the Boring Stuff
+Chapter 7 - Regex
+
+Practice Projects
+Regex Version of strip()
+
+write a function that takes a string, and optionally 1 argument
+if no argument provided (i.e. default) strip leading and trailing
+spaces from string, return string.
+
+if argument provided, then strip the characters specified in the argument
+(not sure if characters are stripped from start and end of string,
+or removed entirely from the string)
+'''
+
+import re
+
+def stripper(stringToStrip, characters = ' '):
+	stripRegex = re.compile(characters)
+
+	if characters == ' ':
+		pass
+	else:
+		strippedString = stripRegex.sub('',stringToStrip)
+		return strippedString
+
+
+print (stripper('  abcd fgh  '))
+print (stripper('  accdggccd', 'c'))
